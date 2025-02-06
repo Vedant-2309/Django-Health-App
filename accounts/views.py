22e44eb0a7786ddebc27e3c62b687e12c1dc7302
Vedant-2309 <Vedant-2309@users.noreply.github.com>
2025-02-06 11:11:25 +0000

    Added a home page and configured urls

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ def signup_view(request):
         if form.is_valid():
             form.save()
             return redirect('login')
+        
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
@@ -25,3 +26,7 @@ def login_view(request):
     else:
         form = LoginForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def home_view(request):
+    return render(request, 'home.html')
